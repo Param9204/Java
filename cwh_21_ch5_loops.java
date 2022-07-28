9f03949d957879adfcdae7287f8f7a3405b3d8fe
Param9204 <109071960+Param9204@users.noreply.github.com>
2022-07-28 22:37:27 +0530

    Update cwh_21_ch5_loops.java

--- cwh_21_ch5_loops.java
+++ cwh_21_ch5_loops.java
@@ -6,7 +6,7 @@ public class cwh_21_ch5_loops {
 
         System.out.println("Using Loops:");
         int i = 100;
-        while(i<=200){de
+        while(i<=200){
             System.out.println(i);
             i++;
         }
